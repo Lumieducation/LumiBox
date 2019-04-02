0a8dc8e8dc8ec18b1198a2053254e401bc9860cb
JPSchellenberg <jps@Lumi.education>
2019-04-02 17:13:09 +0200

    Update webpack-config

---
+++ client/config/webpack.dev.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './client/boot.tsx',
+    mode: 'development',
+    devServer: {
+        contentBase: './build',
+        historyApiFallback: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            lib: path.resolve(__dirname, '../../lib'),
+            client: path.resolve(__dirname, '../../client')
+        }
+    },
+    output: {
+        filename: 'client.js',
+        path: path.resolve(__dirname, '../../build')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'client/index.html'
+        }),
+        new CopyPlugin([{ from: 'client/assets', to: 'assets' }])
+    ]
+};
