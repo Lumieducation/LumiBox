84f1cba44a6a40e73aa0e2af91131d518ceadd3e
JPSchellenberg <jps@Lumi.education>
2019-04-03 13:33:16 +0200

    Tools: Update reducers and reducer-tests

--- lib/tools/reducer/list.ts
+++ lib/tools/reducer/list.ts
@@ -6,20 +6,30 @@ import * as actions from '../actions';
 export default function(state: ITool[] = [], action): ITool[] {
     try {
         switch (action.type) {
-
             case actions.TOOLS_GET_TOOLS_SUCCESS:
-                return unionBy(action.payload, state, '_id')
+                return unionBy(action.response, state, '_id');
 
             case actions.TOOLS_START_TOOL_SUCCESS:
+                return state.map(tool =>
+                    tool._id === action.tool_id
+                        ? assign({}, tool, { status: 'running' })
+                        : tool
+                );
 
-                return state.map(tool => tool._id === action.payload._id ? assign({}, tool, { status: 'running'}): tool);
-           
-                case actions.TOOLS_STOP_TOOL_SUCCESS:
-                return state.map(tool => tool._id === action.payload._id ? assign({}, tool, { status: 'stopped'}): tool);
+            case actions.TOOLS_STOP_TOOL_SUCCESS:
+                return state.map(tool =>
+                    tool._id === action.tool_id
+                        ? assign({}, tool, { status: 'stopped' })
+                        : tool
+                );
 
             case actions.TOOLS_START_TOOL_REQUEST:
             case actions.TOOLS_STOP_TOOL_REQUEST:
-                return state.map(tool => tool._id === action.payload.tool_id ? assign({}, tool, { status: 'pending'}) : tool)
+                return state.map(tool =>
+                    tool._id === action.tool_id
+                        ? assign({}, tool, { status: 'pending' })
+                        : tool
+                );
 
             default:
                 return state;
