9d9f90e2de2b4854a39be36b737a61edeed8885b
Nikolas Martens <Nikolas.M@rtens.org>
2019-04-07 00:45:58 +0200

    [#10] Some more refactoring

--- server/src/server.js
+++ server/src/server.js
@@ -10,6 +10,8 @@ const exec = require('child_process').exec;
 
 const queries = {
 
+    '/tools': () => null,
+
     '/resources': () =>
         execute('df -h /')
             .then(parseSpace)
@@ -38,10 +40,10 @@ const GB = n => Math.round((n / (1024 * 1024 * 1024)) * 100) / 100 + 'GB';
 
 const execute = command =>
     new Promise((y, n) => {
-        console.log('executing:', command);
+        console.log('### executing:', command);
         exec(command, (err, stdout, stderr) => {
             if (err) return n(stderr.toString());
-            console.log('result:', stdout.toString().trim(), stderr.toString());
+            console.log('### result:', stdout.toString().trim(), stderr.toString());
             y(stdout.toString().trim());
         });
     });
@@ -53,6 +55,8 @@ setInterval(() => update('/resources'), 5000)
  * Infrastructure
  */
 
+/** HTTP API */
+
 const port = process.env.PORT || 4200;
 
 const app = express();
@@ -63,7 +67,6 @@ app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('*', (req, res) => {
     const query = req.path
-
     if (!(query in queries)) return res.status(404).end('Not Found')
 
     queries[query](req)
@@ -72,31 +75,42 @@ app.get('*', (req, res) => {
 
 });
 
+/** Subscriptions */
+
 const subscriptions = Object.keys(queries).reduce((acc, i) => ({ ...acc, [i]: {} }), {})
 
 const hasSubscriptions = query => !!Object.keys(subscriptions[query]).length
 
 const update = query => {
     if (!hasSubscriptions(query)) return;
-     
+
     queries[query]().then(result =>
         Object.values(subscriptions[query]).forEach(client =>
             client.emit('update', { query, result })))
 }
 
+const subscribe = (query, client) => {
+    if (!(query in subscriptions)) return console.error('Not found: ' + query)
+    subscriptions[query][client.id] = client
+    update(query)
+    console.log('subscribed', query, client.id)
+}
+
+const unsubscribe = (query, client) => {
+    if (!(query in subscriptions)) return console.error('Not found: ' + query)
+    delete subscriptions[query][client.id]
+    console.log('unsubscribed', query, client.id)
+}
+
+const unsubscribeAll = client => {
+    Object.keys(subscriptions).forEach(query => {
+        if (client.id in subscriptions[query]) unsubscribe(query, client)
+    })
+}
+
 const io = socket(server);
 io.on('connection', client => {
-    client.on('subscribe', query => {
-        if (!(query in subscriptions)) return console.error('Not found: ' + query)
-        subscriptions[query][client.id] = client
-        update(query)
-        console.log('subscribed', query, client.id)
-    });
-    client.on('unsubscribe', query => {
-        if (!(query in subscriptions)) return console.error('Not found: ' + query)
-        delete subscriptions[query][client.id]
-        console.log('unsubscribed', query, client.id)
-    });
-    client.on('disconnect', () =>
-        Object.values(subscriptions).forEach(sub => delete sub[client.id]))
+    client.on('subscribe', query => subscribe(query, client));
+    client.on('unsubscribe', query => unsubscribe(query, client));
+    client.on('disconnect', () => unsubscribeAll(client))
 });
